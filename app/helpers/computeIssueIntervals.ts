313d9ae78ae7a8a302468dddfbc90e2ef8ba3587
Duncan Leo <hello@duncanleo.me>
2025-05-17 00:35:59 +0800

    feat: support issues with rrule

---
+++ app/helpers/computeIssueIntervals.ts
@@ -0,0 +1,35 @@
+import { DateTime, Interval } from 'luxon';
+import { assert } from '../util/assert';
+import type { IssueRef } from '~/types';
+import { rrulestr } from 'rrule';
+
+export function computeIssueIntervals(issue: IssueRef): Interval[] {
+  if (issue.endAt == null) {
+    return [];
+  }
+
+  const startAt = DateTime.fromISO(issue.startAt).setZone('Asia/Singapore');
+  assert(startAt.isValid);
+  const endAt = DateTime.fromISO(issue.endAt).setZone('Asia/Singapore');
+  assert(endAt.isValid);
+
+  const issueIntervals: Interval[] = [];
+
+  if (issue.type === 'maintenance' && issue.rrule != null) {
+    const rruleSet = rrulestr(issue.rrule);
+    for (const dt of rruleSet.all()) {
+      const dtStart = DateTime.fromISO(dt.toISOString())
+        .toUTC()
+        .setZone(rruleSet.options.tzid ?? 'Asia/Singapore', {
+          keepLocalTime: true,
+        });
+      assert(dtStart.isValid);
+      const dtEnd = dtStart.plus(endAt.diff(startAt));
+      issueIntervals.push(Interval.fromDateTimes(dtStart, dtEnd));
+    }
+  } else {
+    issueIntervals.push(Interval.fromDateTimes(startAt, endAt));
+  }
+
+  return issueIntervals;
+}
