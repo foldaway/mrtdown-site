96b876961373a740635179b3f58719c9410d03b7
Duncan Leo <hello@duncanleo.me>
2026-01-24 11:24:22 +0800

    feat: proxy PostHog requests using Vercel Rewrites

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -8,11 +8,21 @@ import posthog from 'posthog-js';
 const { VITE_PUBLIC_POSTHOG_KEY, VITE_PUBLIC_POSTHOG_HOST } = import.meta.env;
 
 if (VITE_PUBLIC_POSTHOG_KEY != null && VITE_PUBLIC_POSTHOG_HOST != null) {
+  let api_host = VITE_PUBLIC_POSTHOG_HOST;
+  let ui_host: string | undefined;
+  let debug = true;
+  if (import.meta.env.PROD) {
+    api_host = '/api/ph';
+    ui_host = VITE_PUBLIC_POSTHOG_HOST;
+    debug = false;
+  }
+
   posthog.init(VITE_PUBLIC_POSTHOG_KEY, {
-    api_host: VITE_PUBLIC_POSTHOG_HOST,
+    api_host,
+    ui_host,
     defaults: '2026-01-30',
     __add_tracing_headers: [window.location.host, 'localhost'],
-    debug: import.meta.env.DEV,
+    debug,
   });
 }
 
