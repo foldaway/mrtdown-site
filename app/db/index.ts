c83f1be21f265cb31de498f58db3f4ed83b544bd
Duncan Leo <hello@duncanleo.me>
2026-05-12 18:42:01 +0800

    Move runtime DB bindings out of foundation

--- app/db/index.ts
+++
@@ -1,7 +0,0 @@
-import { env } from 'cloudflare:workers';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import * as schema from './schema';
-
-export function getDb() {
-  return drizzle(env.HYPERDRIVE.connectionString, { schema });
-}
